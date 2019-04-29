a9d18439a0ba80b0052d951fc6c657a6eed4a3f1
Aleksei <a.a.shejfler@gmail.com>
2019-04-29 09:55:46 +0300

    init

---
+++ src/test/java/com/rootbr/interview/MainTests.java
@@ -0,0 +1,13 @@
+package com.rootbr.interview;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTests {
+
+  @Test
+  public void test() {
+      assertThat(true).isTrue();
+  }
+}
