7935bc4cb6b000377e92a3048d94a117ba3a7499
rootbr <a.a.shejfler@gmail.com>
2019-04-29 11:47:11 +0300

    add mockito

--- src/test/java/com/rootbr/interview/MainTests.java
+++ src/test/java/com/rootbr/interview/MainTests.java
@@ -1,14 +1,26 @@
 package com.rootbr.interview;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class MainTests {
+  public String foo() {
+    return "s";
+  }
 
   @Test
   public void test() {
-//    var b = true;
-//    assertThat(b).isTrue();
+    List list = mock(ArrayList.class);
+    when(list.get(anyInt())).thenReturn(new Object());
+    var b = true;
+    assertThat(b).isTrue();
+    assertThat(list.get(1)).isNotNull();
   }
 }
