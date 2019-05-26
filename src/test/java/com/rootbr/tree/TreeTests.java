98f27ba007e182f0936267ab4759791c2db15f14
rootbr <a.a.shejfler@gmail.com>
2019-05-26 08:43:11 +0300

    рефакторинг

--- src/test/java/com/rootbr/tree/TreeTests.java
+++ src/test/java/com/rootbr/tree/TreeTests.java
@@ -8,41 +8,60 @@ import org.junit.jupiter.api.Test;
 
 public class TreeTests {
 
+  private static final int NODE_PARENT = 5;
+  private static final int NODE_WITHOUT_LISTS = 4;
+  private static final int NODE_ONLY_RIGHT_NODE = 1;
+  private static final int NODE_ONLY_LEFT_NODE = 10;
+  private static final int NODE_WITH_BOTH_NODE = 7;
+  private static final int ANY_VALUE = 11;
+
   @Test
   @DisplayName("после добавления нового элемента, нода находится в дереве и её значение соответствует вставленному")
   public void test1() {
-    final var tree = new Node<Integer>(null);
+    final var tree = defaultTree();
 
-    tree.put(1);
+    tree.put(ANY_VALUE);
 
-    assertThat(tree.get(1)).isNotNull();
-    assertThat(tree.get(1).value).isEqualTo(1);
+    assertThat(tree.get(ANY_VALUE)).isNotNull();
+    assertThat(tree.get(ANY_VALUE).value).isEqualTo(ANY_VALUE);
   }
 
   @Test
   @DisplayName("после добавления нескольких элементов, они все находятся в дереве")
   public void test2() {
-    final var tree = new Node<Integer>(null);
+    final Node<Integer> tree = defaultTree();
 
-    tree.put(2);
-    tree.put(3);
-    tree.put(1);
+    tree.put(123);
+    tree.put(321);
+    tree.put(231);
 
-    assertThat(tree.get(1)).isNotNull();
-    assertThat(tree.get(2)).isNotNull();
-    assertThat(tree.get(3)).isNotNull();
+    assertThat(tree.get(123)).isNotNull();
+    assertThat(tree.get(321)).isNotNull();
+    assertThat(tree.get(231)).isNotNull();
   }
 
   @Test
-  @DisplayName("после удаления элемента, он не находится в дереве")
+  @DisplayName("после удаления элемента без листьев, он не находится в дереве")
   public void test3() {
+    final Node<Integer> tree = defaultTree();
+
+    tree.delete(NODE_WITHOUT_LISTS);
+
+    assertThat(tree.get(NODE_WITHOUT_LISTS)).isNull();
+  }
+
+  private Node<Integer> defaultTree() {
     final var tree = new Node<Integer>(null);
-    tree.put(2);
+    tree.put(NODE_PARENT);
     tree.put(3);
-    tree.put(1);
-
-    tree.delete(3);
-
-    assertThat(tree.get(3)).isNull();
+    tree.put(NODE_ONLY_RIGHT_NODE);
+    tree.put(2);
+    tree.put(NODE_WITHOUT_LISTS);
+    tree.put(NODE_WITH_BOTH_NODE);
+    tree.put(NODE_ONLY_LEFT_NODE);
+    tree.put(8);
+    tree.put(9);
+    tree.put(6);
+    return tree;
   }
 }
